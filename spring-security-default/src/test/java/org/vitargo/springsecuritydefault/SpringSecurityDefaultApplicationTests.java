67889354bf8ef410b1444965906ff1660bbf1b46
Redko Viktoriia <redkov@supportyourapp.com>
2023-02-09 10:03:54 +0200

    Minor changes

---
+++ spring-security-default/src/test/java/org/vitargo/springsecuritydefault/SpringSecurityDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vitargo.springsecuritydefault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDefaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
